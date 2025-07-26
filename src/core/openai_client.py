be870f6f39baee9f4c01e2521c5ea0f4b25498ab
Addison Nicholson <mraddison.nicholson@gmail.com>
2025-07-26 23:17:59 +1200

    Switched from st.secrets to os.environ for env vars

--- src/core/openai_client.py
+++ src/core/openai_client.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from openai import OpenAI
+import os
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
 __all__ = ["client"]
